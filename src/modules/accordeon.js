9c80af81ebe606cd51fe811aec35bb027940ca82
etozhenerk <etozhenerk@yandex.ru>
2021-01-16 20:47:08 +0300

    сделал блок с аккордеоном

---
+++ src/modules/accordeon.js
@@ -0,0 +1,26 @@
+const accordeon = () => {
+  const accordeonBlock = document.querySelector('.accordeon'),
+  elements = accordeonBlock.querySelectorAll('.element'),
+  elementsContent = accordeonBlock.querySelectorAll('.element-content');
+
+  console.log(elementsContent);
+
+  accordeonBlock.addEventListener('click', e => {
+    const target = e.target,
+    parent = target.closest('.element');
+
+    if(parent){
+      elements.forEach((elem, i) => {
+        elem.classList.remove('active');
+        elementsContent[i].style.display = 'none';
+        if(elem === parent){
+          elem.classList.add('active');
+          elementsContent[i].style.display = 'block';
+        }
+      });
+    }
+
+  });
+};
+
+export default accordeon;
