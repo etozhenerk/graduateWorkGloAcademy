70ae6fad70ad70c598927c53048cfa56dfb71189
etozhenerk <etozhenerk@yandex.ru>
2021-01-16 19:19:49 +0300

    Карусель в блоке Наши услуги

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@
 import togglePopUp from './modules/togglePopUp';
 import animateTopMenu from './modules/topMenu';
 import slider from './modules/slider';
+import servicesCarousel from './modules/servicesCarousel';
 
 togglePopUp();
 animateTopMenu();
 slider();
+
+
+const options = {
+  main: '.services-elements', 
+  wrap: '.services-carousel',
+  prev: '.arrow-left',
+  next: '.arrow-right',
+  slidesToShow: 3,
+  infinity: true,
+  responsive: [{
+    breakpoint: 1024,
+    slideToShow: 2
+  },
+  {
+    breakpoint: 576,
+    slideToShow: 1
+  }]
+}
+
+const carousel = new servicesCarousel(options);
+
+carousel.init();
